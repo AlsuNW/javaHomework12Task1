af2381671458ec516f51e4331853bf09add5b7ba
Alsu <alsu.bikkulova@gmail.com>
2023-07-18 11:07:46 +0100

    Tests added

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -103,9 +103,10 @@ public class TodosTest {
     }
 
     @Test
-    public void shouldSearchTasksIfThereIsAFewOfAKind() {
+    public void shouldFindOnly1Task() {
         SimpleTask simpleTask1 = new SimpleTask(5, "Позвонить родителям");
         SimpleTask simpleTask2 = new SimpleTask(58, "Купить продукты");
+        SimpleTask simpleTask3 = new SimpleTask(5555, "Написать эссе");
 
         String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
         Epic epic = new Epic(55, subtasks);
@@ -121,11 +122,72 @@ public class TodosTest {
 
         todos.add(simpleTask1);
         todos.add(simpleTask2);
+        todos.add(simpleTask3);
         todos.add(epic);
         todos.add(meeting);
 
         Task[] expected = {simpleTask2};
         Task[] actual = todos.search("Купить продукты");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindAFewTasks() {
+        SimpleTask simpleTask1 = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask2 = new SimpleTask(58, "Купить продукты");
+        SimpleTask simpleTask3 = new SimpleTask(555, "Купить продукты");
+        SimpleTask simpleTask4 = new SimpleTask(5558, "Купить продукты");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+        todos.add(simpleTask4);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask2, simpleTask3, simpleTask4};
+        Task[] actual = todos.search("Купить продукты");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFind0Tasks() {
+        SimpleTask simpleTask1 = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask2 = new SimpleTask(58, "Купить продукты");
+        SimpleTask simpleTask3 = new SimpleTask(555, "Купить продукты");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Написать эссе");
         Assertions.assertArrayEquals(expected, actual);
     }
 }
